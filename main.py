e1c7d9b5093ce2e4dd3598b50223286ee67b284a
KimMinSoo <minsoo9374@gmail.com>
2023-06-08 16:47:46 +0900

    Revert "커밋4"
    This reverts commit 8de4fd5cd1de09708232671c2c617eb7407ab794.

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ def sub_func(n1, n2) :
 def mul_func(n1, n2) :
     return n1 * n2
 
-def div_func(n1, n2) :
-    return n1 / n2
-
 #전역 변수부
 num1, num2, res = 100, 200, 0
 
@@ -23,6 +20,3 @@ print(num1, '-', num2, '=', res)
 
 res = mul_func(num1, num2)
 print(num1, '*', num2, '=', res)
-
-res = div_func(num1, num2)
-print(num1, '/', num2, '=', res)
